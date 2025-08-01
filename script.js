6e43e68ccd41710e84ca54e996792e4d99086eee
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-08-01 22:01:05 +0200

    Page layout complete. Function created for creating cards.

--- script.js
+++ script.js
@@ -6,7 +6,48 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  // Create card container
+  const cardContainer = document.createElement('div')
+  cardContainer.className = "cardContainer";
+
+  episodeList.forEach((episode) => cardContainer.append(makeEpisodeCard(episode)));
+  rootElem.append(cardContainer)
 }
+
+function makeEpisodeCard({ name, season, number, image, summary }) {
+  // Create card
+  const episodeCard = document.createElement("div");
+  episodeCard.className = "episode-card";
+
+  // Create title
+  const episodeTitle = document.createElement("h2");
+  episodeTitle.className = "episode-title";
+
+  episodeTitle.textContent = `${name} - S${pad(season)}E${pad(number)}`;
+
+  // Create image
+  const episodeImg = document.createElement("img");
+  episodeImg.className = "episode-img";
+  episodeImg.src = image.medium;
+  episodeImg.alt = `${name} thumbnail`;
+
+  // Add summary
+  const episodeSummary = document.createElement("div");
+  episodeSummary.className = "episode-summary";
+  episodeSummary.innerHTML = summary;
+
+  // Building card
+  episodeCard.append(episodeTitle, episodeImg, episodeSummary);
+
+  return episodeCard;
+}
+
+//helper function
+function pad(num) {
+  return num.toString().padStart(2, "0");
+}
+
+console.log(getOneEpisode());
 
 window.onload = setup;
